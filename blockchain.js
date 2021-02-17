6260f3ba8ebd3afd8eef76c9870fe0e9af0a907e
Dakota States <dakota.states@gmail.com>
2021-02-16 20:09:17 -0800

    first commit

---
+++ blockchain.js
@@ -0,0 +1,40 @@
+var  createHash = require('crypto');
+const url = require('url');
+
+class Block
+{
+    constructor(index,timestamp,transactions,proof,previous_hash)
+    {
+        this._index = index;
+        this._timestamp = timestamp;
+        this._transactions = transactions;
+        this._proof = proof;
+        this._previous_hash = previous_hash;
+    }
+
+    get timestamp()
+    {
+         return this._timestamp;
+    }
+
+    get transactions()
+    {
+         return this._transactions;
+    }
+
+    get proof()
+    {
+         return this._proof;
+    }
+
+    get previous_hash()
+    {
+         return this._previous_hash;
+    }
+
+    get index()
+    {
+         return this._index;
+    }
+
+}
